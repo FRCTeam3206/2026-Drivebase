e1b3673f667708f6591d7747cbd37da45940677c
fortnite misogynist <audreyromain0@gmail.com>
2026-01-14 19:21:52 -0600

    forogt spotless apply,,, whoopsie daisies!!!

--- src/frc/robot/BuildConstants.java
+++ src/frc/robot/BuildConstants.java
@@ -1,8 +1,6 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026-Drivebase";
@@ -15,5 +13,5 @@ public final class BuildConstants {
   public static final long BUILD_UNIX_TIME = 1768439551121L;
   public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
--- src/test/java/DriveSubsystemTest.java
+++ src/test/java/DriveSubsystemTest.java
@@ -1,30 +1,31 @@
-import org.junit.jupiter.api.Assertions;
 import frc.robot.subsystems.DriveSubsystem;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
-
+import org.junit.jupiter.api.Test;
 
 @Tag("real life")
 public class DriveSubsystemTest {
-    private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
 
-    @BeforeEach
-    void setup() {
+  @BeforeEach
+  void setup() {}
 
-    }
-
-    @Test
-    void drive() {
-        final double initialX = 0;
-        final double initialY = 0;
-        driveSubsystem.runOnce(()->{
-            driveSubsystem.drive(0.1, 0.1, 0, false);
-        }).withTimeout(5).andThen(()->{
-            driveSubsystem.drive(0, 0, 0, false);
-            Assertions.assertTrue(driveSubsystem.getPose().getX() > initialX);
-            Assertions.assertTrue(driveSubsystem.getPose().getY() > initialY);
-        });
-    }
+  @Test
+  void drive() {
+    final double initialX = 0;
+    final double initialY = 0;
+    driveSubsystem
+        .runOnce(
+            () -> {
+              driveSubsystem.drive(0.1, 0.1, 0, false);
+            })
+        .withTimeout(5)
+        .andThen(
+            () -> {
+              driveSubsystem.drive(0, 0, 0, false);
+              Assertions.assertTrue(driveSubsystem.getPose().getX() > initialX);
+              Assertions.assertTrue(driveSubsystem.getPose().getY() > initialY);
+            });
+  }
 }
