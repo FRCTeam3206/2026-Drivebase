58a37ffbae638babbe8013bd5f6db1ba76a2c404
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2026-01-21 12:51:34 -0600

    Spotless.

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,7 +1,6 @@
 package frc.robot;
 
 import com.revrobotics.spark.FeedbackSensor;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
